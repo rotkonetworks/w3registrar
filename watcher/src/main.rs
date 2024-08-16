bdc08d931e2b203c6663938808c6e504caf244d5
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-08-16 18:50:59 +0200

    watcher: hardcode log level to INFO for now

--- watcher/src/main.rs
+++ watcher/src/main.rs
@@ -1,17 +1,16 @@
 use anyhow::Result;
 use tracing_subscriber::fmt::format::FmtSpan;
 use tracing::Level;
-use std::fs;
 use serde::Deserialize;
+
+use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = Config::load()?;
 
     tracing_subscriber::fmt()
-        .with_max_level(config.log_level
-            .parse::<Level>()
-            .expect("Failed to parse log level"))
+        .with_max_level(Level::INFO)
         .with_span_events(FmtSpan::CLOSE)
         .init();
 
@@ -22,7 +21,6 @@ async fn main() -> Result<()> {
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    pub log_level: String,
     pub endpoint: String,
     pub registrar_index: u32,
     pub keystore_path: String,
