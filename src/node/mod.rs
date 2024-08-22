1acead4216dc1aedbacfb067a5d387d9b69b3e95
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-08-22 13:40:27 +0200

    node: delete everything but the block fetching code

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -9,8 +9,6 @@ use subxt::utils::H256;
 use anyhow::Result;
 use serde::Deserialize;
 
-use std::collections::HashMap;
-
 pub type RegistrarIndex = u32;
 pub type MaxFee = f64;
 
@@ -55,10 +53,6 @@ impl Client {
         Ok(Self::new(inner, config.registrar_index))
     }
 
-    pub async fn exec(&self, _cmd: Command) -> Result<()> {
-        todo!()
-    }
-
     pub async fn fetch_block(&self, hash: &str) -> Result<Block> {
         let hash = hash.parse::<H256>()?;
         let block = self.inner.blocks().at(hash).await?;
@@ -77,23 +71,6 @@ impl Client {
             hash: hash.to_string(),
             events,
         })
-    }
-
-    pub async fn fetch_contact_details(&self, id: &AccountId) -> Result<Option<FieldMap>> {
-        let query = api::storage()
-            .identity()
-            .identity_of(id);
-
-        let identity = self.inner
-            .storage()
-            .at_latest()
-            .await?
-            .fetch(&query)
-            .await?;
-
-        Ok(identity.map(|(reg, _)| {
-            decode_identity_info(reg.info)
-        }))
     }
 }
 
@@ -156,13 +133,6 @@ pub struct Block {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Command {
-    Batch(Vec<Self>),
-    RequestJudgement(AccountId, MaxFee),
-    ProvideJudgement(AccountId, Judgement),
-}
-
-#[derive(Debug, Clone, PartialEq)]
 pub enum Event {
     IdentitySet(AccountId),
     IdentityCleared(AccountId),
@@ -185,96 +155,4 @@ impl Event {
             }
         }
     }
-}
-
-#[derive(Debug, Copy, Clone, PartialEq)]
-pub enum Judgement {
-    Good,
-    Bad,
-}
-
-//------------------------------------------------------------------------------
-
-pub type FieldMap = HashMap<FieldKey, String>;
-
-// TODO: Name?
-pub type Field = (FieldKey, String);
-
-// TODO: Add PgpFingerprint
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub enum FieldKey {
-    Display,
-    Legal,
-    Web,
-    Matrix,
-    Email,
-    Image,
-    Twitter,
-    Github,
-    Discord,
-}
-
-fn decode_identity_info(info: api::IdentityInfo) -> FieldMap {
-    use FieldKey::*;
-    let mut fields = HashMap::new();
-    decode_identity_field_into(Display, info.display, &mut fields);
-    decode_identity_field_into(Legal, info.legal, &mut fields);
-    decode_identity_field_into(Web, info.web, &mut fields);
-    decode_identity_field_into(Matrix, info.matrix, &mut fields);
-    decode_identity_field_into(Email, info.email, &mut fields);
-    decode_identity_field_into(Image, info.image, &mut fields);
-    decode_identity_field_into(Twitter, info.twitter, &mut fields);
-    decode_identity_field_into(Github, info.github, &mut fields);
-    decode_identity_field_into(Discord, info.discord, &mut fields);
-    fields
-}
-
-fn decode_identity_field_into(key: FieldKey, value: api::Data, fields: &mut FieldMap) {
-    if let Some(s) = decode_string_data(value) {
-        fields.insert(key, s);
-    }
-}
-
-fn decode_string_data(data: api::Data) -> Option<String> {
-    use api::Data::*;
-    match data {
-        Raw0(b) => Some(string_from_bytes(&b)),
-        Raw1(b) => Some(string_from_bytes(&b)),
-        Raw2(b) => Some(string_from_bytes(&b)),
-        Raw3(b) => Some(string_from_bytes(&b)),
-        Raw4(b) => Some(string_from_bytes(&b)),
-        Raw5(b) => Some(string_from_bytes(&b)),
-        Raw6(b) => Some(string_from_bytes(&b)),
-        Raw7(b) => Some(string_from_bytes(&b)),
-        Raw8(b) => Some(string_from_bytes(&b)),
-        Raw9(b) => Some(string_from_bytes(&b)),
-        Raw10(b) => Some(string_from_bytes(&b)),
-        Raw11(b) => Some(string_from_bytes(&b)),
-        Raw12(b) => Some(string_from_bytes(&b)),
-        Raw13(b) => Some(string_from_bytes(&b)),
-        Raw14(b) => Some(string_from_bytes(&b)),
-        Raw15(b) => Some(string_from_bytes(&b)),
-        Raw16(b) => Some(string_from_bytes(&b)),
-        Raw17(b) => Some(string_from_bytes(&b)),
-        Raw18(b) => Some(string_from_bytes(&b)),
-        Raw19(b) => Some(string_from_bytes(&b)),
-        Raw20(b) => Some(string_from_bytes(&b)),
-        Raw21(b) => Some(string_from_bytes(&b)),
-        Raw22(b) => Some(string_from_bytes(&b)),
-        Raw23(b) => Some(string_from_bytes(&b)),
-        Raw24(b) => Some(string_from_bytes(&b)),
-        Raw25(b) => Some(string_from_bytes(&b)),
-        Raw26(b) => Some(string_from_bytes(&b)),
-        Raw27(b) => Some(string_from_bytes(&b)),
-        Raw28(b) => Some(string_from_bytes(&b)),
-        Raw29(b) => Some(string_from_bytes(&b)),
-        Raw30(b) => Some(string_from_bytes(&b)),
-        Raw31(b) => Some(string_from_bytes(&b)),
-        Raw32(b) => Some(string_from_bytes(&b)),
-        _ => Option::None,
-    }
-}
-
-fn string_from_bytes(bytes: &[u8]) -> String {
-    std::str::from_utf8(&bytes).unwrap_or("").to_string()
 }
