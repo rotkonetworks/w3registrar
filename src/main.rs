a6b54851318862dfd322d997a891d571cb694de2
Yassine <yassinebenarbia980@gmail.com>
2024-10-10 01:39:31 +0300

    refacor: move event listening and event management to a separate functions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ mod node;
 mod repo;
 mod config;
 
-use tokio::sync::mpsc;
+use anyhow::Result;
+use node::Event;
+use subxt::{OnlineClient, SubstrateConfig};
+use tokio::sync::mpsc::{self, Receiver, Sender};
 use tracing::Level;
 use tracing_subscriber::fmt::format::FmtSpan;
 
@@ -20,14 +23,15 @@ async fn main() -> Result<()> {
     run_watcher(config.watcher).await
 }
 
-async fn run_watcher(cfg: WatcherConfig) -> Result<()> {
-    let client = node::Client::from_url(cfg.endpoint.as_str()).await?;
-    let client_clone = client.clone();
+async fn event_listener(tx: Sender<Event>, client: OnlineClient<SubstrateConfig>) -> Result<()> {
+    node::fetch_events(&client, &tx).await
+}
 
-    let (tx, mut rx) = mpsc::channel(100);
-
-    tokio::spawn(async move { node::fetch_events(&client, &tx).await.unwrap(); });
-
+async fn manage_events(
+    rx: &mut Receiver<Event>,
+    cfg: WatcherConfig,
+    client: OnlineClient<SubstrateConfig>,
+) -> Result<()> {
     while let Some(event) = rx.recv().await {
         use node::Event::*;
         match event {
@@ -37,7 +41,7 @@ async fn run_watcher(cfg: WatcherConfig) -> Result<()> {
             }
             JudgementRequested(who, ri) => {
                 if ri == cfg.registrar_index {
-                    let reg = node::get_registration(&client_clone, &who).await?;
+                    let reg = node::get_registration(&client, &who).await?;
                     if reg.has_paid_fee() {
                         println!("Judgement requested by {}: {:#?}", who, reg.identity);
                     }
@@ -50,7 +54,7 @@ async fn run_watcher(cfg: WatcherConfig) -> Result<()> {
             }
             JudgementGiven(who, ri) => {
                 if ri == cfg.registrar_index {
-                    let reg = node::get_registration(&client_clone, &who).await?;
+                    let reg = node::get_registration(&client, &who).await?;
                     if let Some(judgement) = reg.last_judgement() {
                         println!("Judgement given to {}: {:?}", who, judgement);
                     }
@@ -61,7 +65,13 @@ async fn run_watcher(cfg: WatcherConfig) -> Result<()> {
     Ok(())
 }
 
+async fn run_watcher(cfg: WatcherConfig) -> Result<()> {
+    let client = node::Client::from_url(cfg.endpoint.as_str()).await?;
+    let client_clone = client.clone();
 
+    let (tx, mut rx) = mpsc::channel(100);
 
+    tokio::spawn(event_listener(tx, client));
 
+    manage_events(&mut rx, cfg, client_clone).await
 }
