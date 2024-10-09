6ad7231ac5dc95e39a135cdf1e387509bb358a72
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-10-09 23:59:43 +0200

    Merge `node::api` into `node`

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -1,13 +1,22 @@
 mod substrate;
-mod api;
+use substrate::api;
 
-pub use api::{AccountId, Client, IdentityEvent, Registration, Judgement};
+pub use subxt::utils::AccountId32 as AccountId;
+pub use api::runtime_types::pallet_identity::pallet::Event as IdentityEvent;
+pub use api::runtime_types::people_rococo_runtime::people::IdentityInfo;
 
+use subxt::{OnlineClient, SubstrateConfig};
 use anyhow::{anyhow, Result};
 use async_stream::try_stream;
 use tokio_stream::Stream;
 
 pub type RegistrarIndex = u32;
+
+pub type Registration = api::runtime_types::pallet_identity::types::Registration<u128, IdentityInfo>;
+
+pub type Judgement = api::runtime_types::pallet_identity::types::Judgement<u128>;
+
+pub type Client = OnlineClient<SubstrateConfig>;
 
 pub async fn subscribe_to_identity_events(
     client: &Client
