0a474af5495f0d67dce5bebff78fa98d7120e5e4
Yassine <yassinebenarbia980@gmail.com>
2024-10-09 20:21:40 +0300

    chor: add `flake.nix` to enable reproducible dev environement

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        unstable = import <nixpkgs-unstable> {};
+        naersk-lib = pkgs.callPackage naersk { };
+
+        rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+        pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+        rustVersion = "1.79.0";
+        rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+        extensions = [
+          "rust-src" # for rust-analyzer
+          "rustc"
+          "cargo"
+        ];};
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ rust pkg-config openssl shim-unsigned ];
+        };
+      });
+}
