7ba50c50fbaa41b272ae18c11c83fbb22f458130
Yassine <yassinebenarbia980@gmail.com>
2024-10-10 16:45:57 +0300

    refactor: move event management to `node::event_manger`

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
+mod config;
 mod matrix;
 mod node;
-mod repo;
-mod config;
 
 use anyhow::Result;
-use node::Event;
-use subxt::{OnlineClient, SubstrateConfig};
-use tokio::sync::mpsc::{self, Receiver, Sender};
+use config::{Config, WatcherConfig};
 use tracing::Level;
 use tracing_subscriber::fmt::format::FmtSpan;
-
-use config::{Config, WatcherConfig};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -29,57 +24,8 @@ async fn run_watcher(cfg: WatcherConfig) -> Result<()> {
     let client = node::Client::from_url(cfg.endpoint.as_str()).await?;
 
     let event_stream = node::subscribe_to_identity_events(&client).await?;
+
     tokio::pin!(event_stream);
 
-    while let Some(event_res) = event_stream.next().await {
-        use node::IdentityEvent::*;
-
-        let event = event_res?;
-        match event {
-            | IdentitySet { who }
-            | IdentityCleared { who, .. }
-            | IdentityKilled { who, .. } => {
-                println!("Identity changed for {}", who);
-            }
-            JudgementRequested { who, registrar_index } => {
-                if registrar_index == cfg.registrar_index {
-                    let reg = node::get_registration(&client, &who).await?;
-                    // TODO: Clean this up.
-                    let has_paid_fee = reg.judgements.0.iter()
-                        .any(|(_, j)| matches!(j, node::Judgement::FeePaid(_)));
-                    if has_paid_fee {
-                        println!("Judgement requested by {}: {:#?}", who, reg.info);
-                    }
-                }
-            }
-            JudgementUnrequested { who, registrar_index } => {
-                if registrar_index == cfg.registrar_index {
-                    println!("Judgement unrequested by {}", who);
-                }
-            }
-            JudgementGiven(who, ri) => {
-                if ri == cfg.registrar_index {
-                    let reg = node::get_registration(&client, &who).await?;
-                    if let Some(judgement) = reg.last_judgement() {
-                        println!("Judgement given to {}: {:?}", who, judgement);
-                    }
-                }
-            }
-            _ => {
-                println!("{:?}", event);
-            }
-        }
-    }
-    Ok(())
-}
-
-async fn run_watcher(cfg: WatcherConfig) -> Result<()> {
-    let client = node::Client::from_url(cfg.endpoint.as_str()).await?;
-    let client_clone = client.clone();
-
-    let (tx, mut rx) = mpsc::channel(100);
-
-    tokio::spawn(event_listener(tx, client));
-
-    manage_events(&mut rx, cfg, client_clone).await
+    node::event_manager(event_stream, cfg, client).await
 }
--- src/node/mod.rs
+++ src/node/mod.rs
@@ -1,24 +1,31 @@
 mod api;
 
+use std::pin::Pin;
+
 pub use api::runtime_types::pallet_identity::pallet::Event as IdentityEvent;
 pub use api::runtime_types::people_rococo_runtime::people::IdentityInfo;
+use futures::StreamExt;
 pub use subxt::utils::AccountId32 as AccountId;
 
 use anyhow::{anyhow, Result};
 use async_stream::try_stream;
+use subxt::{OnlineClient, SubstrateConfig};
 use tokio_stream::Stream;
+
+use crate::config::WatcherConfig;
 
 pub type Client = subxt::OnlineClient<subxt::SubstrateConfig>;
 
-pub type Registration = api::runtime_types::pallet_identity::types::Registration<u128, IdentityInfo>;
+pub type Registration =
+    api::runtime_types::pallet_identity::types::Registration<u128, IdentityInfo>;
 
 pub type Judgement = api::runtime_types::pallet_identity::types::Judgement<u128>;
 
 pub type RegistrarIndex = u32;
 
 pub async fn subscribe_to_identity_events(
-    client: &Client
-) -> Result<impl Stream<Item=Result<IdentityEvent>>> {
+    client: &Client,
+) -> Result<impl Stream<Item = Result<IdentityEvent>>> {
     let mut block_stream = client.blocks().subscribe_finalized().await?;
 
     Ok(try_stream! {
@@ -46,4 +53,63 @@ pub async fn get_registration(client: &Client, who: &AccountId) -> Result<Regist
         None => Err(anyhow!("No registration found for {}", who)),
         Some((reg, _)) => Ok(reg),
     }
+}
+
+pub async fn event_manager<'a>(
+    mut event_stream: Pin<&'a mut impl futures::Stream<Item = Result<IdentityEvent>>>,
+    cfg: WatcherConfig,
+    client: OnlineClient<SubstrateConfig>,
+) -> Result<()> {
+    while let Some(event_res) = event_stream.next().await {
+        use api::runtime_types::pallet_identity::pallet::Event::*;
+
+        let event = event_res?;
+        match event {
+            IdentitySet { who } | IdentityCleared { who, .. } | IdentityKilled { who, .. } => {
+                println!("Identity changed for {}", who);
+            }
+            JudgementRequested {
+                who,
+                registrar_index,
+            } => {
+                if registrar_index == cfg.registrar_index {
+                    let reg = get_registration(&client, &who).await?;
+                    // TODO: Clean this up.
+                    let has_paid_fee = reg
+                        .judgements
+                        .0
+                        .iter()
+                        .any(|(_, j)| matches!(j, Judgement::FeePaid(_)));
+                    if has_paid_fee {
+                        println!("Judgement requested by {}: {:#?}", who, reg.info);
+                    }
+                }
+            }
+            JudgementUnrequested {
+                who,
+                registrar_index,
+            } => {
+                if registrar_index == cfg.registrar_index {
+                    println!("Judgement unrequested by {}", who);
+                }
+            }
+            JudgementGiven {
+                target,
+                registrar_index,
+            } => {
+                if registrar_index == cfg.registrar_index {
+                    let reg = get_registration(&client, &target).await?;
+                    // TODO: Clean this up.
+                    if let Some(judgement) = reg.judgements.0.last().map(|(j, _)| *j) {
+                        println!("Judgement given to {}: {:?}", target, judgement);
+                    }
+                }
+            }
+            _ => {
+                println!("{:?}", event);
+            }
+        }
+    }
+
+    Ok(())
 }
