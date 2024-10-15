28376e111820949753440dd85b670612977dde6a
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-10-15 17:05:35 +0200

    Extract `node::events_from_block()`

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -1,15 +1,22 @@
+#![allow(dead_code)]
+
 mod api;
+
+use anyhow::{Result, anyhow};
+use async_stream::try_stream;
+use subxt::SubstrateConfig;
+use tokio_stream::Stream;
 
 pub use api::Event;
 pub use api::runtime_types::pallet_identity::pallet::Event as IdentityEvent;
 pub use api::runtime_types::people_kusama_runtime::people::IdentityInfo;
 pub use subxt::utils::AccountId32 as AccountId;
 
-use anyhow::{Result, anyhow};
-use async_stream::try_stream;
-use tokio_stream::Stream;
+pub type Client = subxt::OnlineClient<SubstrateConfig>;
 
-pub type Client = subxt::OnlineClient<subxt::SubstrateConfig>;
+pub type Block = subxt::blocks::Block<SubstrateConfig, Client>;
+
+pub type BlockHash = <SubstrateConfig as subxt::Config>::Hash;
 
 pub type Registration =
 api::runtime_types::pallet_identity::types::Registration<u128, IdentityInfo>;
@@ -22,16 +29,24 @@ pub async fn subscribe_to_events(
     let mut block_stream = client.blocks().subscribe_finalized().await?;
 
     Ok(try_stream! {
-        while let Some(block_res) = block_stream.next().await {
-            let block = block_res?;
-            for event_res in block.events().await?.iter() {
-                let event_details = event_res?;
-                if let Ok(event) = event_details.as_root_event::<Event>() {
-                    yield event;
-                }
+        while let Some(item) = block_stream.next().await {
+            let block = item?;
+            for event in events_from_block(block).await?.into_iter() {
+                yield event;
             }
         }
     })
+}
+
+pub async fn events_from_block(block: Block) -> Result<Vec<Event>> {
+    let mut events = Vec::new();
+    for item in block.events().await?.iter() {
+        let details = item?;
+        if let Ok(event) = details.as_root_event::<Event>() {
+            events.push(event);
+        }
+    }
+    Ok(events)
 }
 
 pub async fn get_registration(
