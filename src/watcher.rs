d430d5f6db83cc8fc48dfbe50188625856afdfac
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-10-15 17:12:26 +0200

    Refactor `watcher`
    Split run() up into smaller functions that can be used independently.

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,9 +1,12 @@
+#![allow(dead_code)]
+
 use crate::node;
 
 use serde::Deserialize;
 use tokio_stream::StreamExt;
 use tracing::info;
-use crate::node::Event;
+
+use node::{Client, Event};
 
 pub type RegistrarIndex = u32;
 
@@ -16,36 +19,43 @@ pub struct Config {
 }
 
 pub async fn run(cfg: Config) -> anyhow::Result<()> {
-    let client = node::Client::from_url(cfg.endpoint.as_str()).await?;
-    let ri = cfg.registrar_index;
+    let client = Client::from_url(cfg.endpoint.as_str()).await?;
+    watch_node(&client, cfg.registrar_index).await
+}
 
+async fn watch_node(client: &Client, ri: RegistrarIndex) -> anyhow::Result<()> {
     let event_stream = node::subscribe_to_events(&client).await?;
     tokio::pin!(event_stream);
 
     while let Some(item) = event_stream.next().await {
-        use node::IdentityEvent::*;
-
         let event = item?;
+        handle_event(&client, ri, event).await?;
+    }
 
-        match event {
-            Event::Identity(JudgementRequested { who, registrar_index })
-            if registrar_index == ri => {
-                let reg = node::get_registration(&client, &who).await?;
+    Ok(())
+}
 
-                // TODO: Clean this up.
-                let has_paid_fee = reg
-                    .judgements
-                    .0
-                    .iter()
-                    .any(|(_, j)| matches!(j, node::Judgement::FeePaid(_)));
+async fn handle_event(client: &Client, ri: RegistrarIndex, event: Event) -> anyhow::Result<()> {
+    use node::IdentityEvent::*;
 
-                if has_paid_fee {
-                    info!("Judgement requested by {}: {:#?}", who, reg.info);
-                }
+    match event {
+        Event::Identity(JudgementRequested { who, registrar_index })
+        if registrar_index == ri => {
+            let reg = node::get_registration(&client, &who).await?;
+
+            // TODO: Clean this up.
+            let has_paid_fee = reg
+                .judgements
+                .0
+                .iter()
+                .any(|(_, j)| matches!(j, node::Judgement::FeePaid(_)));
+
+            if has_paid_fee {
+                info!("Judgement requested by {}: {:#?}", who, reg.info);
             }
-            _ => {
-                info!("Ignoring {:?}", event);
-            }
+        }
+        _ => {
+            info!("Ignoring {:?}", event);
         }
     }
 
