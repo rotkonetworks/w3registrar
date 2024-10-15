27d7f1ea88fbf7a4c9f3f8ecabe3a6455b92ff37
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-10-15 16:44:12 +0200

    Only handle `JudgementRequested` event for now

--- src/watcher.rs
+++ src/watcher.rs
@@ -16,48 +16,34 @@ pub struct Config {
 
 pub async fn run(cfg: Config) -> anyhow::Result<()> {
     let client = node::Client::from_url(cfg.endpoint.as_str()).await?;
+    let ri = cfg.registrar_index;
 
     let event_stream = node::subscribe_to_identity_events(&client).await?;
     tokio::pin!(event_stream);
 
-    while let Some(event_res) = event_stream.next().await {
+    while let Some(item) = event_stream.next().await {
         use node::IdentityEvent::*;
 
-        let event = event_res?;
+        let event = item?;
+
         match event {
-            IdentitySet { who } | IdentityCleared { who, .. } | IdentityKilled { who, .. } => {
-                info!("Identity changed for {}", who);
-            }
-            JudgementRequested { who, registrar_index } => {
-                if registrar_index == cfg.registrar_index {
-                    let reg = node::get_registration(&client, &who).await?;
-                    // TODO: Clean this up.
-                    let has_paid_fee = reg
-                        .judgements
-                        .0
-                        .iter()
-                        .any(|(_, j)| matches!(j, node::Judgement::FeePaid(_)));
-                    if has_paid_fee {
-                        info!("Judgement requested by {}: {:#?}", who, reg.info);
-                    }
-                }
-            }
-            JudgementUnrequested { who, registrar_index } => {
-                if registrar_index == cfg.registrar_index {
-                    info!("Judgement unrequested by {}", who);
-                }
-            }
-            JudgementGiven { target, registrar_index } => {
-                if registrar_index == cfg.registrar_index {
-                    let reg = node::get_registration(&client, &target).await?;
-                    // TODO: Clean this up.
-                    if let Some(judgement) = reg.judgements.0.last().map(|(j, _)| *j) {
-                        info!("Judgement given to {}: {:?}", target, judgement);
-                    }
+            JudgementRequested { who, registrar_index }
+            if registrar_index == ri => {
+                let reg = node::get_registration(&client, &who).await?;
+
+                // TODO: Clean this up.
+                let has_paid_fee = reg
+                    .judgements
+                    .0
+                    .iter()
+                    .any(|(_, j)| matches!(j, node::Judgement::FeePaid(_)));
+
+                if has_paid_fee {
+                    info!("Judgement requested by {}: {:#?}", who, reg.info);
                 }
             }
             _ => {
-                info!("{:?}", event);
+                info!("Ignoring {:?}", event);
             }
         }
     }
