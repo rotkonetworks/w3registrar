c3ec97ae0c2867ee4ed3c4882e60fefc3bf18e56
Yassine <yassinebenarbia980@gmail.com>
2024-12-02 16:52:10 +0200

    refactor: improve node module functionality and identity handling

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -4,14 +4,19 @@
 pub mod api {}
 
 use anyhow::{anyhow, Result};
-use async_stream::try_stream;
+use sp_core::blake2_256;
+use sp_core::Encode;
+use std::str::FromStr;
 use subxt::ext::sp_core::sr25519::Pair as Sr25519Pair;
 use subxt::ext::sp_core::Pair;
 use subxt::SubstrateConfig;
-use tokio_stream::Stream;
+use tracing::info;
+
+use api::runtime_types::pallet_identity::types::Registration;
+use api::runtime_types::people_rococo_runtime::people::IdentityInfo;
+use subxt::utils::AccountId32;
 
 pub use api::*;
-pub use api::IdentityEvent as Event;
 
 pub type Client = subxt::OnlineClient<SubstrateConfig>;
 
@@ -21,73 +26,41 @@ pub type BlockHash = <SubstrateConfig as subxt::Config>::Hash;
 
 type PairSigner = subxt::tx::PairSigner<SubstrateConfig, Sr25519Pair>;
 
-#[derive(Debug)]
-pub struct JudgementEnvelope {
-    pub registrar_index: RegistrarIndex,
-    pub target: AccountId,
-    pub judgement: Judgement,
-    pub identity_hash: Identity,
-}
-
-pub async fn provide_judgement(
-    client: &Client,
-    seed_phrase: &str,
-    env: JudgementEnvelope
-) -> Result<()> {
-    let pair = Sr25519Pair::from_phrase(&seed_phrase, None)?;
-    let signer = PairSigner::new(pair.0);
-
-    let call = api::provide_judgement(
-        env.registrar_index,
-        Target::Id(env.target.clone()),
-        env.judgement,
-        env.identity_hash,
-    );
-
-    client.tx()
-        .sign_and_submit_then_watch_default(&call, &signer)
-        .await?
-        .wait_for_finalized_success()
-        .await?;
-
-    Ok(())
-}
-
-pub async fn subscribe_to_events(
-    client: &Client
-) -> Result<impl Stream<Item = Result<Event>>> {
-    let mut block_stream = client.blocks().subscribe_finalized().await?;
-
-    Ok(try_stream! {
-        while let Some(item) = block_stream.next().await {
-            let block = item?;
-            for event in events_from_block(block).await?.into_iter() {
-                yield event;
-            }
-        }
-    })
-}
-
-pub async fn events_from_block(block: Block) -> Result<Vec<Event>> {
-    let mut events = Vec::new();
-    for item in block.events().await?.iter() {
-        let details = item?;
-        if let Ok(event) = details.as_root_event::<RuntimeEvent>() {
-            match event {
-                RuntimeEvent::Identity(e) => events.push(e)
-            }
-        }
-    }
-    Ok(events)
-}
 
 pub async fn get_registration(
-    client: &Client, who: &AccountId
-) -> Result<Registration> {
+    client: &Client,
+    who: &AccountId32,
+) -> Result<Registration<u128, IdentityInfo>> {
     let storage = client.storage().at_latest().await?;
-    let address = api::identity_of(who);
-    match storage.fetch(&address).await? {
+    let identity = super::node::storage().identity().identity_of(who);
+    info!("identity: {:?}", identity);
+    match storage.fetch(&identity).await? {
         None => Err(anyhow!("No registration found for {}", who)),
         Some((reg, _)) => Ok(reg),
     }
+}
+
+pub async fn register_identity<'a>(who: AccountId32, reg_index: u32) -> anyhow::Result<&'a str> {
+    let client = Client::from_url("wss://dev.rotko.net/people-rococo").await?;
+    let registration = get_registration(&client, &who).await.unwrap();
+    let hash = hex::encode(blake2_256(&registration.info.encode()));
+    let judgement = api::tx().identity().provide_judgement(
+        reg_index,
+        subxt::utils::MultiAddress::Address32(who.to_owned().0),
+        runtime_types::pallet_identity::types::Judgement::KnownGood,
+        api::identity::calls::types::provide_judgement::Identity::from_str(&hash).unwrap(),
+    );
+
+    let singer: subxt::tx::signer::PairSigner<SubstrateConfig, subxt::ext::sp_core::sr25519::Pair> = {
+        let acc = subxt::ext::sp_core::sr25519::Pair::from_string("//ALICE", None)?;
+        subxt::tx::PairSigner::new(acc)
+    };
+
+    let conf = subxt::config::substrate::SubstrateExtrinsicParamsBuilder::new().build();
+    client
+        .tx()
+        .sign_and_submit(&judgement, &singer, conf)
+        .await
+        .unwrap();
+    Ok("KnownGood")
 }
---
+++ src/token.rs
@@ -0,0 +1,51 @@
+use rand::{distributions::Alphanumeric, Rng};
+use serde::{Deserialize, Serialize};
+
+pub trait AuthToken: PartialEq + Eq {
+    /// Generates a token that constitutes of the account and
+    /// the expected message to recive from the account
+    async fn generate() -> Token;
+    fn show(&self) -> String;
+}
+
+#[derive(PartialEq, Eq, Debug, Clone, Serialize, Deserialize)]
+pub struct Token {
+    /// Expected message to recieve
+    expected_message: String,
+}
+
+impl From<String> for Token {
+    fn from(value: String) -> Self {
+        Self {
+            expected_message: value,
+        }
+    }
+}
+
+impl<'a> From<&'a str> for Token {
+    fn from(value: &'a str) -> Self {
+        Self {
+            expected_message: value.to_owned(),
+        }
+    }
+}
+
+impl AuthToken for Token {
+    /// Generates a [Token] for a specific [AccountType], this token
+    /// constituted of the [AccountType] and the expecetd message as a
+    /// [String] 10 characters long
+    async fn generate() -> Token {
+        let s: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(10)
+            .map(char::from)
+            .collect();
+        Token {
+            expected_message: s,
+        }
+    }
+
+    fn show(&self) -> String {
+        self.expected_message.to_owned()
+    }
+}
