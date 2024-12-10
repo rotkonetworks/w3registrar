ca313dc34cad3399441d69e0136e3cc72f78798c
Yassine <yassinebenarbia980@gmail.com>
2024-12-10 05:43:49 +0200

    chor: update rust version to `1.83.0`
    updated rust version version from `1.81.0` to `1.83.0` while also adding
    `rust-analyzer`

--- flake.nix
+++ flake.nix
@@ -13,11 +13,12 @@
 
         rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
         pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-        rustVersion = "1.81.0";
+        rustVersion = "1.83.0";
         rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
         extensions = [
           "rust-src" # for rust-analyzer
           "rustc"
+          "rust-analyzer"
           "cargo"
         ];};
       in
