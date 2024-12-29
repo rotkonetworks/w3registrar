1447b9f09a24d7ae702e1a8750eb1d708ca77e44
hitchhooker <tommi@niemi.lol>
2024-12-30 02:37:46 +0700

    use less opinionated host instead of ip

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,7 +2,7 @@
 use crate::node::identity::events::judgement_requested::RegistrarIndex;
 use anyhow::anyhow;
 use std::fs;
-use toml;
+use url::Url;
 
 use serde::Deserialize;
 
@@ -37,7 +37,7 @@ pub struct MatrixConfig {
 
 #[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
 pub struct RedisConfig {
-    pub ip: [u8; 4],
+    pub host: String,
     pub port: u16,
     pub username: String,
     pub password: String,
@@ -46,7 +46,7 @@ pub struct RedisConfig {
 impl Default for RedisConfig {
     fn default() -> Self {
         Self {
-            ip: [127, 0, 0, 1],
+            host: "127.0.0.1".to_string(),
             port: 6379,
             username: String::new(),
             password: String::new(),
@@ -55,24 +55,16 @@ impl Default for RedisConfig {
 }
 
 impl RedisConfig {
-    pub fn to_full_domain(&self) -> String {
+    pub fn to_url(&self) -> Result<Url, url::ParseError> {
+        let mut url = Url::parse(&format!("redis://{}:{}/", self.host, self.port))?;
+
         if !self.username.is_empty() || !self.password.is_empty() {
-            return format!(
-                "redis://{}:{}@{}.{}.{}.{}:{}/",
-                self.username,
-                self.password,
-                self.ip[0],
-                self.ip[1],
-                self.ip[2],
-                self.ip[3],
-                self.port
-            );
-        } else {
-            return format!(
-                "redis://{}.{}.{}.{}:{}/",
-                self.ip[0], self.ip[1], self.ip[2], self.ip[3], self.port
-            );
+            url.set_username(&self.username)
+                .map_err(|()| url::ParseError::IdnaError)?;
+            url.set_password(Some(&self.password))
+                .map_err(|()| url::ParseError::IdnaError)?;
         }
+        Ok(url)
     }
 }
 
@@ -85,14 +77,14 @@ pub struct WatcherConfig {
 
 #[derive(Debug, Deserialize)]
 pub struct WebsocketConfig {
-    pub ip: [u8; 4],
+    pub host: String,
     pub port: u16,
 }
 
 impl Default for WebsocketConfig {
     fn default() -> Self {
         Self {
-            ip: [127, 0, 0, 1],
+            host: "127.0.0.1".to_string(),
             port: 8080,
         }
     }
