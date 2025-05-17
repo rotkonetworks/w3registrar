9ce4e4c9cff312a2bd4cb3ff4656d1c44246a417
71walceli <71walceli@71walceli.dev>
2025-05-17 23:11:15 +0000

    fix: replace unwrap with expect for better error handling in database connection

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -10,7 +10,10 @@ pub fn get_connection() -> PgConnection {
     let config = GLOBAL_CONFIG.get().expect("GLOBAL_CONFIG must be set up!");
     let database_url = config.postgres.database_url.clone();
 
-    let pg_connection = PgConnection::establish(&database_url).unwrap();
+    let pg_connection = PgConnection::establish(&database_url).expect(&format!(
+        "Error connecting to {}",
+        database_url
+    ));
     info!("Connected to PostgreSQL database at {}", database_url);
     pg_connection
 }
