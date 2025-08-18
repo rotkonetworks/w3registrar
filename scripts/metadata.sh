ac0619a94be65a691e55617bb5f92ef7da29a4ce
yassinebenarbia <yassine.bna@proton.me>
2025-08-18 19:13:41 +0200

    fix: create metadata directory is present in script
    Changes:
    Make sure that meatadata directory is present by crating it using `mkdir`

--- scripts/metadata.sh
+++ scripts/metadata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-METADATA_DIR="$(cd "$(dirname "$0")"/../metadata && pwd)"
+METADATA_DIR="$(dirname "$0")/../metadata"
+mkdir -p "$METADATA_DIR"
+METADATA_DIR="$(cd "$METADATA_DIR" && pwd)"
 subxt metadata -f bytes --url wss://people-kusama.dotters.network > "$METADATA_DIR/people_kusama.scale" && subxt metadata -f bytes --url wss://people-paseo.dotters.network > "$METADATA_DIR/people_paseo.scale" && subxt metadata -f bytes --url wss://people-polkadot.dotters.network > "$METADATA_DIR/people_polkadot.scale"
